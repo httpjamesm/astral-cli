106402caaede29a7d2ae3cb15b3e01762e464f85
httpjamesm <github@httpjames.space>
2021-04-07 16:19:36 -0400

    help menu

--- astralcli.py
+++ astralcli.py
@@ -63,3 +63,33 @@ class main():
         embeds().editEmbedPreset(args[argPos + 1])
     if "--clear" in args:
         accounts().clearData()
+    if "--help" in args:
+        print(
+            """
+AstralCLI is a command line interface for Astral.cool's API.
+
+
+General Usage:
+    --upload <file path>                Upload a file to Astral.
+    --domains                           View all Astral domains.
+        
+Settings:
+    --login <username> <password>       Login to Astral with your credentials.
+
+    --settings                          View all of your settings.
+    --change <setting> <value>          Change a specific setting.
+
+    --embeds                            View current embed profiles.
+    --new-embed                         Create a new embed profile.
+    --del-embed <profile #>             Delete an embed profile.
+    --edit-embed <profile #>            Edit an embed profile
+
+    --add-random-domain <domain>        Add a random domain to your list.
+    --del-random-domain <domain>        Delete a random domain from your list.
+
+    --regen-upkey                       Regenerate your upload key.
+    --clear                             Clear all local account data.
+
+Are you a developer? Contribute to the AstralCLI project here: https://github.com/httpjamesm/astral-cli.
+            """
+        )
