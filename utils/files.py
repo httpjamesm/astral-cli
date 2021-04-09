2224209054387e3dd9b4a82bc68e05de151c5ad6
httpjamesm <github@httpjames.space>
2021-04-08 21:50:32 -0400

    fix incorrect b to mb conversion, change size formatting

--- utils/files.py
+++ utils/files.py
@@ -8,11 +8,12 @@ from utils.accounts import accounts
 
 class files():
     def viewFiles(self, page):
+        # Request headers with the user access token
         authorization = {
             "Authorization": "Bearer " + accounts().getAccessToken()
         }
         filesRequest = requests.get(data.configdata["credentials"]["endpoint"] + "files/" + str(page), headers=authorization).json()
         for x in filesRequest["data"]:
             fileList = []
-            fileList.extend(["ID: " + x["id"], "Server Name: " + x["filename"], "Original Name: " + x["originalName"], "Size (MB): " + str(x["size"]/10000), "Timestamp: " + x["createdAt"], f"Deletion URL: {{}}files/delete?key={{}}".format(data.configdata["credentials"]["endpoint"], x["deletionKey"]),"\n"])
+            fileList.extend(["ID: " + x["id"], "Server Name: " + x["filename"], "Original Name: " + x["originalName"], "Size: " + str(round(x["size"]/1000000,2)) + " MB", "Timestamp: " + x["createdAt"], f"Deletion URL: {{}}files/delete?key={{}}".format(data.configdata["credentials"]["endpoint"], x["deletionKey"]),"\n"])
             print('\n'.join(fileList))
